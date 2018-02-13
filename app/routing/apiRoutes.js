d160cf16ffd694d063ccb6f629db1d992b40006c
Mateo Vargas <mateovargas@Mateos-MBP.san.rr.com>
2018-02-13 15:01:06 -0800

    Routing worked, working on dropdowns

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var path = require("path");
+var bodyParser = require("body-parser");
 var friendsList = require('../data/friends.js');
 var apiRouter = express.Router();
+
+apiRouter.use(bodyParser.urlencoded({ extended: false }));
+apiRouter.use(bodyParser.json());
 
 apiRouter.get("/api/friends", function (req, res) {
     res.json(friendsList);
 });
 
-/**apiRouter.post("/api/friends", function (req, res) {
+apiRouter.post("/api/friends", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     console.log(req.body);
     var newFriend = req.body;
-
     console.log(newFriend);
-    res.json(friends)
+    res.json(friendsList)
     
-});**/
+});
 
 module.exports = apiRouter;
